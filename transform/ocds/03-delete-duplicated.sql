3c0568a0c7b5f0d5a77ccc3bb4d59a15930c3134
Yohanna Lisnichuk <yohanitalisnichuk@gmail.com>
2020-12-17 17:03:03 -0300

    Remove remove duplicated
    Signed-off-by: Yohanna Lisnichuk <yohanitalisnichuk@gmail.com>

--- transform/ocds/03-delete-duplicated.sql
+++ transform/ocds/03-delete-duplicated.sql
@@ -1,26 +1,4 @@
-delete from ocds.award where data_id in (
-select a.data_id from ocds.procurement a
-join ocds.procurement b on a.ocid = b.ocid and a.id < b.id);
 
-delete from ocds.award_items where data_id in (
-select a.data_id from ocds.procurement a
-join ocds.procurement b on a.ocid = b.ocid and a.id < b.id);
-
-delete from ocds.tender_items where data_id in (
-select a.data_id from ocds.procurement a
-join ocds.procurement b on a.ocid = b.ocid and a.id < b.id);
-
-delete from ocds.contract where data_id in (
-select a.data_id from ocds.procurement a
-join ocds.procurement b on a.ocid = b.ocid and a.id < b.id);
-
-delete from ocds.parties where data_id in (
-select a.data_id from ocds.procurement a
-join ocds.procurement b on a.ocid = b.ocid and a.id < b.id);
-
-delete from ocds.procurement where data_id in (
-select a.data_id from ocds.procurement a
-join ocds.procurement b on a.ocid = b.ocid and a.id < b.id);
 
 refresh materialized view ocds.unique_suppliers;
 
