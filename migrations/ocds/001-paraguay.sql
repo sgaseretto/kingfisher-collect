2729804c99343c1aa1342cee3692aae2c9c1eef6
Matt <matt31sanabria@gmail.com>
2021-06-16 11:59:49 -0400

    Se cambian nombres de columnas y tablas

--- migrations/ocds/001-paraguay.sql
+++ migrations/ocds/001-paraguay.sql
@@ -182,7 +182,7 @@ alter table ocds.procurement add column if not exists tender_procurementmethod t
 -- Creacion de una vista de objetos de gastos unicos ordenados ascendentemente
 CREATE OR REPLACE VIEW ocds.view_objeto_gasto AS
     SELECT distinct(g.codigo_objeto_gasto), g.descripcion_objeto_gasto
-    FROM hacienda.gasto g
+    FROM gasto g
     ORDER BY g.codigo_objeto_gasto ASC;
 
 -- vista de adjudicaciones
@@ -191,9 +191,9 @@ CREATE OR REPLACE VIEW ocds.view_adjudicaciones AS
        p.data_id as data_id,
        p.budget::json->0->'classifications'->>'anio' as anho,
        p.budget::json->0->'classifications'->>'nivel' as nivel,
-       e.descripcion_nivel as descripcion_nivel,
+       e.descripcionNivel as descripcion_nivel,
        p.budget::json->0->'classifications'->>'entidad' as entidad,
-       e.descripcion as descripcion_entidad,
+       e.descripcionEntidad as descripcion_entidad,
        og.codigo_objeto_gasto as codigo_objeto_gasto,
        og.descripcion_objeto_gasto as descripcion_objeto_gasto,
        a.currency as moneda,
@@ -210,8 +210,8 @@ CREATE OR REPLACE VIEW ocds.view_adjudicaciones AS
        a.award_id
     FROM ocds.procurement p
     LEFT JOIN ocds.award a ON p.data_id = a.data_id
-    LEFT JOIN hacienda.entidad e ON e.nivel::text = p.budget::json->0->'classifications'->>'nivel'
-                                        AND e.codigo::text = p.budget::json->0->'classifications'->>'entidad'
+    LEFT JOIN entidad_entidad e ON e.nivel::text = p.budget::json->0->'classifications'->>'nivel'
+                                        AND e.codigoEntidad::text = p.budget::json->0->'classifications'->>'entidad'
     INNER JOIN ocds.view_objeto_gasto og ON og.codigo_objeto_gasto::text = p.budget::json->0->'classifications'->>'objeto_gasto'
     INNER JOIN ocds.contract c ON c.data_id = p.data_id
     WHERE e.anio = 2020;
@@ -222,9 +222,9 @@ CREATE OR REPLACE VIEW ocds.view_planificaciones AS
        p.data_id as data_id,
        p.budget::json->0->'classifications'->>'anio' as anho,
        p.budget::json->0->'classifications'->>'nivel' as nivel,
-       e.descripcion_nivel as descripcion_nivel,
+       e.descripcionNivel as descripcion_nivel,
        p.budget::json->0->'classifications'->>'entidad' as entidad,
-       e.descripcion as descripcion_entidad,
+       e.descripcionEntidad as descripcion_entidad,
        a.currency as moneda,
        p.tender_procurementmethoddetails as modalidad,
        p.tender_mainprocurementcategorydetails as categoria,
@@ -244,8 +244,8 @@ CREATE OR REPLACE VIEW ocds.view_planificaciones AS
        a.award_id
     FROM ocds.procurement p
     LEFT JOIN ocds.award a ON a.data_id = p.data_id
-    LEFT JOIN hacienda.entidad e ON e.nivel::text = p.budget::json->0->'classifications'->>'nivel'
-                                        AND e.codigo::text = p.budget::json->0->'classifications'->>'entidad'
+    LEFT JOIN entidad_entidad e ON e.nivel::text = p.budget::json->0->'classifications'->>'nivel'
+                                        AND e.codigoEntidad::text = p.budget::json->0->'classifications'->>'entidad'
     INNER JOIN ocds.view_objeto_gasto og ON og.codigo_objeto_gasto::text = p.budget::json->0->'classifications'->>'objeto_gasto'
     INNER JOIN ocds.contract c ON c.data_id = p.data_id
     WHERE e.anio = 2020;
